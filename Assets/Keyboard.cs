21bd3d910981ef693e73186677a2b4192d92aec2
DESKTOP-QVOJ483\iibi203 <jseong444@naver.com>
2023-02-16 19:54:24 +0900

    키보드연습

---
+++ Assets/Keyboard.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Keyboard : MonoBehaviour
+{
+    public TMP_InputField inputField;
+    public GameObject normalButton;
+    public GameObject capsButtons;
+    private bool caps;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        caps = false;
+        
+    }
+
+    public void InsertChar(string c)
+    {
+        inputField.text += c;
+    }
+
+    public void DeleteChar()
+    {
+        if (inputField.text.Length > 0)
+        { 
+            inputField.text = inputField.text.Substring(0, inputField.text.Length - 1);
+        }
+    }
+
+    public void InsertSpace()
+    {
+        inputField.text += " ";
+    }
+
+    public void CapsPressed()
+    {
+        if (!caps)
+        {
+            normalButton.SetActive(false);
+            capsButtons.SetActive(true);
+            caps = true;
+        }
+        else { 
+            capsButtons.SetActive(false);
+            normalButton.SetActive(true);
+            caps = false;
+        }
+    }
+}
